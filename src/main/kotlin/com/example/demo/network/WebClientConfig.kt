5f1dcb19782ac1b5c9d7d9a86ad59b757f61a8cc
TM <tjmcard@gmail.com>
2024-02-19 21:12:40 -0800

    Increase base request memory size

--- src/main/kotlin/com/example/demo/network/WebClientConfig.kt
+++ src/main/kotlin/com/example/demo/network/WebClientConfig.kt
@@ -3,6 +3,7 @@ package com.example.demo.network
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
 
 /**
@@ -19,6 +20,7 @@ class WebClientConfig {
     @Bean
     fun MlbWebClient(): WebClient {
         return WebClient.builder()
+            .exchangeStrategies(strategies)
             .baseUrl(mlbUrl)
             .build()
     }
@@ -26,8 +28,15 @@ class WebClientConfig {
     @Bean
     fun WeatherWebClient(): WebClient {
         return WebClient.builder()
+            .exchangeStrategies(strategies)
             .baseUrl(weatherUrl)
             .build()
     }
+
+
+    // Create a custom ExchangeStrategies object with an increased buffer size
+    val strategies = ExchangeStrategies.builder()
+        .codecs { configurer -> configurer.defaultCodecs().maxInMemorySize(1024 * 1024 * 10) } // Set max buffer size to 10 MB
+        .build()
 
 }
