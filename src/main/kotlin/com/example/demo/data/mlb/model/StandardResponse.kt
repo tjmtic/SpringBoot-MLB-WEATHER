bf9f61fe9de69019e67ef59bcc7a5cfe26b56be4
TM <tjmcard@gmail.com>
2024-02-19 21:13:22 -0800

    Standard request Implementation

---
+++ src/main/kotlin/com/example/demo/data/mlb/model/StandardResponse.kt
@@ -0,0 +1,3 @@
+package com.example.demo.data.mlb.model
+
+data class StandardResponse(val games: List<Game>?, val venues: List<Venue>?, val team: List<Team?>?, val leagues: List<League?>?, val sport: List<Sport?>?, val divisions: List<Division?>?)
--- src/main/kotlin/com/example/demo/service/mlb/request/standard/StandardRequestImpl.kt
+++ src/main/kotlin/com/example/demo/service/mlb/request/standard/StandardRequestImpl.kt
@@ -1,48 +1,56 @@
 package com.example.demo.service.mlb.request.standard
 
 import com.example.demo.data.mlb.mapper.ResponseMapper
+import com.example.demo.data.mlb.mapper.StandardResponseMapper
+import com.example.demo.data.mlb.model.GenericResponse
+import com.example.demo.data.mlb.model.StandardMapping
+import com.example.demo.data.mlb.model.StandardResponse
 import com.example.demo.service.mlb.MlbServiceException
 import com.example.demo.service.mlb.MlbServiceImpl
 import com.example.demo.service.mlb.request.league.LeagueRequest
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
-/*
-class StandardRequestImpl<T>(val name: String, val webClient: WebClient) {
 
-    fun getAll(): StandardResponse<T> {
+class StandardRequestImpl(val name: String, val webClient: WebClient, val error: String) {
+
+    companion object {
+        const val NOT_FOUND = "Request: Not Found"
+    }
+
+    fun getAll(): StandardResponse {
         try {
-            return getStandardRequest().block() ?: throw MlbServiceException(MlbServiceImpl.NOT_FOUND_LEAGUE)
+            return getStandardRequest().block() ?: throw MlbServiceException(error)
         } catch (e: MlbServiceException){
-            throw MlbServiceException(MlbServiceImpl.NOT_FOUND_TEAM + e.message, e)
+            throw MlbServiceException(error + e.message, e)
         } catch (e: Exception){
             throw MlbServiceException("${e.message}", e)
         }
     }
 
-    private fun getStandardRequest(): Mono<StandardResponse<T>> {
+    private fun getStandardRequest(): Mono<StandardResponse> {
 
         return webClient.get()
             .uri(name)
             .retrieve()
             .bodyToMono(String::class.java)
-            .map{ T::map(it) }
+            .map{ StandardResponseMapper().map(it) }
             .onErrorMap {
                 MlbServiceException("${it.message}", it)
             }
     }
 
-    fun getById(id: String): StandardResponse<T> {
+    fun getById(id: String): StandardResponse {
         try {
-            return getStandardRequest(id).block() ?: throw MlbServiceException(MlbServiceImpl.NOT_FOUND_LEAGUE)
+            return getStandardRequest(id).block() ?: throw MlbServiceException(NOT_FOUND)
         } catch (e: MlbServiceException){
-            throw MlbServiceException(MlbServiceImpl.NOT_FOUND_TEAM + e.message, e)
+            throw MlbServiceException(NOT_FOUND + e.message, e)
         } catch (e: Exception){
             throw MlbServiceException("${e.message}", e)
         }
     }
 
-    private fun getStandardRequest(id: String): Mono<StandardResponse<T>> {
+    /*private fun <T> getStandardRequest(id: String): Mono<List<T?>?> {
 
         return webClient.get()
             .uri("${name}/$id")
@@ -52,6 +60,40 @@ class StandardRequestImpl<T>(val name: String, val webClient: WebClient) {
             .onErrorMap {
                 MlbServiceException("${it.message}", it)
             }
+    }*/
+
+    private fun getStandardRequest(id: String): Mono<StandardResponse> {
+
+        return webClient.get()
+            .uri("${name}/$id")
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .map{ StandardResponseMapper().map(it) }
+            .onErrorMap {
+                MlbServiceException("${it.message}", it)
+            }
     }
-}*/
+
+    fun getCustom(urlPath: String): StandardResponse {
+        try {
+            return getStandardRequestCustom(urlPath).block() ?: throw MlbServiceException(NOT_FOUND)
+        } catch (e: MlbServiceException){
+            throw MlbServiceException(NOT_FOUND + e.message, e)
+        } catch (e: Exception){
+            throw MlbServiceException("${e.message}", e)
+        }
+    }
+
+    private fun getStandardRequestCustom(urlPath: String): Mono<StandardResponse> {
+
+        return webClient.get()
+            .uri(urlPath)
+            .retrieve()
+            .bodyToMono(String::class.java)
+            .map{ StandardResponseMapper().map(it) }
+            .onErrorMap {
+                MlbServiceException("${it.message}", it)
+            }
+    }
+}
 
